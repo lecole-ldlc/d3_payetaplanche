d19dfad5d36059b9411a745d79951032e61ff97f
mickael.adelaide <mickael.adelaide@lecole-ldlc.com>
2017-11-10 10:15:50 +0100

    play w scales

--- src/bubble_chart.js
+++ src/bubble_chart.js
@@ -98,8 +98,8 @@ function bubbleChart() {
     // Sizes bubbles based on area.
     // @v4: new flattened scale names.
     var radiusScale = d3.scalePow()
-      .exponent(3)
-      .range([2, 85])
+      .exponent(-0.5)
+      .range([1, 20])
       .domain([0, maxAmount]);
 
     // Use map() to convert raw data into node data.
@@ -108,7 +108,7 @@ function bubbleChart() {
     var myNodes = rawData.map(function (d) {
       return {
         id: d.id,
-        radius: radiusScale(+d.time/(maxAmount*10)),
+        radius: radiusScale(+d.time),
         value: +d.time,
         name: d.name,
         org: d.who,
